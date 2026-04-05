e3bd3cffce4d2dcc899cbc9486c0b8fb77b3d1ab
Gabriel R. <93205028+gabrielro-dev@users.noreply.github.com>
2026-04-05 12:56:09 -0300

    Update test_timer.py

--- test_timer.py
+++ test_timer.py
@@ -1,1 +1,9 @@
+import subprocess
+import time
+
+def test_timer_runs_for_a_few_seconds():
+    process = subprocess.Popen(["python", "timer.py"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    time.sleep(3)
+    process.terminate()
+    assert process.poll() is not None
 
